ab320073f692c7e058ffbb304778fcdf68b83198
MRITUNJOY SAHA <mritunjoysaha@MRITUNJOYs-MacBook-Air.local>
2026-05-02 04:01:16 +0530

    Made changes to the test-branch-2

--- apps/views.py
+++ apps/views.py
@@ -5,5 +5,4 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome to MyProject!")
 
-def test_branch_2(request):
-    return HttpResponse("This is a test branch view from the test-branch-2.")
+
