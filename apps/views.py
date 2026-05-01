c5b5b624743148c96b25b4bf7dc93ddd6ab2eaf1
MRITUNJOY SAHA <mritunjoysaha@MRITUNJOYs-MacBook-Air.local>
2026-05-02 03:57:53 +0530

    Made new addition from the test-branch-2

--- apps/views.py
+++ apps/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome to MyProject!")
 
-def test_branch_1(request):
-    return HttpResponse("This is a test branch view from the test-branch-1.")
+def test_branch_2(request):
+    return HttpResponse("This is a test branch view from the test-branch-2.")
